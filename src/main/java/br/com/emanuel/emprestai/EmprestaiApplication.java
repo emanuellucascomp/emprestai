1909a7c8f50758c4fd52fbc948a55466b40ebddf
emanuellucascomp <cardgameheritage@gmail.com>
2025-12-01 16:47:09 -0300

    initial commit

---
+++ src/main/java/br/com/emanuel/emprestai/EmprestaiApplication.java
@@ -0,0 +1,13 @@
+package br.com.emanuel.emprestai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmprestaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmprestaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/emanuel/emprestai/EmprestaiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.emanuel.emprestai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmprestaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
